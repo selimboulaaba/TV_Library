02852678364285e90ed7ed716661ccd551ea1894
Selim Boulaaba <selimboulaaba00@gmail.com>
2024-09-08 05:52:58 +0100

    fix import

--- front_library/src/App.jsx
+++ front_library/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-import AddLibrary from "./pages/addLibrary";
+import AddLibrary from "./pages/AddLibrary";
 
 function App() {
 
