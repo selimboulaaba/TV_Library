2371470b8805810d869c013d64f50c874d37b688
Selim Boulaaba <selimboulaaba00@gmail.com>
2024-09-19 04:08:34 +0100

    fix cors

--- front_library/src/services/ShowService.jsx
+++ front_library/src/services/ShowService.jsx
@@ -1,21 +1,12 @@
 import axios from "axios";
 
 const url = import.meta.env.VITE_BACK_URL + "/tvs";
-const headers = {
-    'Content-Type': 'application/json',
-};
 
 export const addToLibrary = async (payload) => {
-    return await axios.post(url, payload, {
-        headers: headers,
-        withCredentials: true,
-    })
+    return await axios.post(url, payload)
 }
 
 export const getShows = async (type) => {
-    return await axios.post(url + "/paginate", { type }, {
-        headers: headers,
-        withCredentials: true,
-    })
+    return await axios.post(url + "/paginate", { type })
 }
 
