8d096f9a55f6bf8ca26bc267a72f6ca29c587a40
Selim Boulaaba <selimboulaaba00@gmail.com>
2024-09-19 13:48:19 +0100

    trying to fix cors

--- back_library/src/main.ts
+++ back_library/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: process.env.FRONT_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept',
   });
